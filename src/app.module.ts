8eb2939f0aa2f74e6284d059a7e28fff8238d382
FranGJ <fransergiogj7@gmail.com>
2023-09-21 19:33:01 -0300

    Conexão com mongoDB realizada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://mongodb:27017/yourdatabase')],
   controllers: [AppController],
   providers: [AppService],
 })
